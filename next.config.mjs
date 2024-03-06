7b4d44e50c9e4d1616e4bed7550db0cb092f5d24
Tayrika <rika13tay@gmail.com>
2024-03-06 17:04:52 +0900

    Add Google image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'logos-world.net',
+                pathname: '**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 function Home() {
   return ( 
     <>
-      <HomeHeader /> 
+      <HomeHeader />
+      <div>
+        <Image src="https://logos-world.net/wp-content/uploads/2020/09/Google-Logo.png" alt="Google logo" width={300} height={300} />
+        <HomeSearch />
+      </div>
     </>
    );
 }
