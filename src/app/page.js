edd8196d27518a479cf5235b490ee75a23faeac1
Tayrika <rika13tay@gmail.com>
2024-03-06 17:27:06 +0900

    Add search button

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ function Home() {
   return ( 
     <>
       <HomeHeader />
-      <div>
+      <div className="flex flex-col items-center mt-24">
         <Image src="https://logos-world.net/wp-content/uploads/2020/09/Google-Logo.png" alt="Google logo" width={300} height={300} />
         <HomeSearch />
       </div>
