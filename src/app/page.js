0964f2a12c68432909fe43cd9428e9b133199e0a
Tayrika <rika13tay@gmail.com>
2024-03-07 13:38:58 +0900

    Add body section

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ function Home() {
     <>
       <HomeHeader />
       <div className="flex flex-col items-center mt-24">
-        <Image src="https://logos-world.net/wp-content/uploads/2020/09/Google-Logo.png" alt="Google logo" width={300} height={300} />
+        <Image src="https://logos-world.net/wp-content/uploads/2020/09/Google-Logo.png" alt="Google logo" width={300} height={300} priority style={{ width: "auto" }} />
         <HomeSearch />
       </div>
     </>
